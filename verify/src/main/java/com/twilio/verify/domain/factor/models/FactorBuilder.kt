6eea0182c5932eb97e3469f07821934969f4b78e
Yeimi Moreno <ymoreno@twilio.com>
2020-01-13 16:49:58 -0500

    Add methods to set builder values

--- verify/src/main/java/com/twilio/verify/domain/factor/models/FactorBuilder.kt
+++ verify/src/main/java/com/twilio/verify/domain/factor/models/FactorBuilder.kt
@@ -8,8 +8,19 @@ import com.twilio.verify.models.FactorType
 internal class FactorBuilder {
 
   lateinit var friendlyName: String
+    private set
   lateinit var type: FactorType
+    private set
   lateinit var binding: Map<String, Any>
+    private set
   lateinit var serviceSid: String
+    private set
   lateinit var userId: String
+    private set
+
+  fun friendlyName(friendlyName: String) = apply { this.friendlyName = friendlyName }
+  fun type(type: FactorType) = apply { this.type = type }
+  fun binding(binding: Map<String, Any>) = apply { this.binding = binding }
+  fun serviceSid(serviceSid: String) = apply { this.serviceSid = serviceSid }
+  fun userId(userId: String) = apply { this.userId = userId }
 }
