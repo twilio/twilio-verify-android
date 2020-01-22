32b0f4a8c611e75683765ee49652b26693e02543
Yeimi Moreno <ymoreno@twilio.com>
2020-01-22 14:05:30 -0500

    Implement factor creation

--- verify/src/main/java/com/twilio/verify/data/KeyStoreAdapter.kt
+++ verify/src/main/java/com/twilio/verify/data/KeyStoreAdapter.kt
@@ -7,31 +7,18 @@ import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import com.twilio.verify.TwilioVerifyException
 import com.twilio.verify.TwilioVerifyException.ErrorCode.KeyStorageError
+import java.security.KeyPair
 import java.security.KeyPairGenerator
 import java.security.KeyStore
 import java.security.Signature
 
 private const val provider = "AndroidKeyStore"
 
-internal class KeyStoreAdapter : KeyStorage {
+internal class KeyStoreAdapter(private val androidKeyStore: AndroidKeyStore = AndroidKeyStore()) :
+    KeyStorage {
   override fun create(alias: String): String {
     return try {
-      val keyPairGenerator = KeyPairGenerator.getInstance(
-          KeyProperties.KEY_ALGORITHM_EC,
-          provider
-      )
-      val parameterSpec = KeyGenParameterSpec.Builder(
-          alias,
-          KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
-      )
-          .run {
-            setDigests(KeyProperties.DIGEST_SHA256)
-            build()
-          }
-
-      keyPairGenerator.initialize(parameterSpec)
-      val keyPair = keyPairGenerator.generateKeyPair()
-      String(keyPair.public.encoded)
+      String(androidKeyStore.createKeyPair(alias).public.encoded)
     } catch (e: Exception) {
       throw TwilioVerifyException(KeyStorageException(e), KeyStorageError)
     }
@@ -41,22 +28,8 @@ internal class KeyStoreAdapter : KeyStorage {
     alias: String,
     message: String
   ): String {
-    try {
-      val ks = KeyStore.getInstance(provider)
-          .apply {
-            load(null)
-          }
-      val entry = ks.getEntry(alias, null)
-      if (entry !is KeyStore.PrivateKeyEntry) {
-        throw IllegalArgumentException("Key entry not found for alias")
-      }
-      return Signature.getInstance("SHA256withECDSA")
-          .run {
-            initSign(entry.privateKey)
-            update(message.toByteArray())
-            sign()
-          }
-          .let { String(it) }
+    return try {
+      androidKeyStore.sign(alias, message)
     } catch (e: Exception) {
       throw TwilioVerifyException(KeyStorageException(e), KeyStorageError)
     }
@@ -69,4 +42,45 @@ class KeyStorageException private constructor(
 ) : Exception(message, exception) {
   constructor(exception: Exception) : this(exception.message, exception)
   constructor(message: String) : this(message, null)
+}
+
+class AndroidKeyStore {
+  fun createKeyPair(alias: String): KeyPair {
+    val keyPairGenerator = KeyPairGenerator.getInstance(
+        KeyProperties.KEY_ALGORITHM_EC,
+        provider
+    )
+    val parameterSpec = KeyGenParameterSpec.Builder(
+        alias,
+        KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+    )
+        .run {
+          setDigests(KeyProperties.DIGEST_SHA256)
+          build()
+        }
+
+    keyPairGenerator.initialize(parameterSpec)
+    return keyPairGenerator.generateKeyPair()
+  }
+
+  fun sign(
+    alias: String,
+    message: String
+  ): String {
+    val ks = KeyStore.getInstance(provider)
+        .apply {
+          load(null)
+        }
+    val entry = ks.getEntry(alias, null)
+    if (entry !is KeyStore.PrivateKeyEntry) {
+      throw IllegalArgumentException("Key entry not found for alias")
+    }
+    return Signature.getInstance("SHA256withRSA")
+        .run {
+          initSign(entry.privateKey)
+          update(message.toByteArray())
+          sign()
+        }
+        .let { String(it) }
+  }
 }
---
+++ verify/src/test/java/com/twilio/verify/domain/TwilioVerifyManagerTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020, Twilio Inc.
+ */
+package com.twilio.verify.domain
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.twilio.verify.TwilioVerifyException
+import com.twilio.verify.TwilioVerifyException.ErrorCode.InputError
+import com.twilio.verify.domain.factor.FactorFacade
+import com.twilio.verify.models.Factor
+import com.twilio.verify.models.PushFactorInput
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class TwilioVerifyManagerTest {
+
+  private val factorFacade: FactorFacade = mock()
+  private val twilioVerifyManager = TwilioVerifyManager(factorFacade)
+
+  @Test
+  fun `Create a factor should call success`() {
+    val factorInput = PushFactorInput("name", "pushToken", "jwt")
+    val expectedFactor: Factor = mock()
+    argumentCaptor<(Factor) -> Unit>().apply {
+      whenever(factorFacade.createFactor(eq(factorInput), capture(), any())).then {
+        firstValue.invoke(expectedFactor)
+      }
+    }
+    twilioVerifyManager.createFactor(factorInput, { factor ->
+      assertEquals(expectedFactor, factor)
+    }, {
+      fail()
+    })
+  }
+
+  @Test
+  fun `Error creating a factor should call error`() {
+    val factorInput = PushFactorInput("name", "pushToken", "jwt")
+    val expectedException: Exception = mock()
+    argumentCaptor<(TwilioVerifyException) -> Unit>().apply {
+      whenever(factorFacade.createFactor(eq(factorInput), any(), capture())).then {
+        firstValue.invoke(TwilioVerifyException(expectedException, InputError))
+      }
+    }
+    twilioVerifyManager.createFactor(factorInput, {
+      fail()
+    }, { exception ->
+      assertEquals(expectedException, exception.cause)
+    })
+  }
+}
