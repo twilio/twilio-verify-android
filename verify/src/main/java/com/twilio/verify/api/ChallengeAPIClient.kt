24608103fbfcbe2a2d223ca36fc6a405dfeaaf95
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-02-20 11:20:37 -0500

    Get challenge

--- verify/src/main/java/com/twilio/verify/api/ChallengeAPIClient.kt
+++ verify/src/main/java/com/twilio/verify/api/ChallengeAPIClient.kt
@@ -5,19 +5,24 @@ import com.twilio.verify.BuildConfig
 import com.twilio.verify.TwilioVerifyException
 import com.twilio.verify.TwilioVerifyException.ErrorCode.NetworkError
 import com.twilio.verify.domain.challenge.models.FactorChallenge
+import com.twilio.verify.domain.factor.models.PushFactor
 import com.twilio.verify.networking.Authorization
+import com.twilio.verify.networking.HttpMethod.Get
 import com.twilio.verify.networking.HttpMethod.Post
 import com.twilio.verify.networking.NetworkAdapter
 import com.twilio.verify.networking.NetworkException
 import com.twilio.verify.networking.NetworkProvider
 import com.twilio.verify.networking.Request
 import com.twilio.verify.networking.RequestHelper
+import org.json.JSONObject
 
 /*
  * Copyright (c) 2020, Twilio Inc.
  */
 internal const val challengeSidPath = "{ChallengeSid}"
 internal const val updateChallengeURL =
+  "${BuildConfig.BASE_URL}Services/$serviceSidPath/Entities/$entitySidPath/Factors/$factorSidPath/Challenges/$challengeSidPath"
+internal const val getChallengeURL =
   "${BuildConfig.BASE_URL}Services/$serviceSidPath/Entities/$entitySidPath/Factors/$factorSidPath/Challenges/$challengeSidPath"
 
 internal class ChallengeAPIClient(
@@ -51,6 +56,30 @@ internal class ChallengeAPIClient(
     }
   }
 
+  fun get(
+    sid: String,
+    pushFactor: PushFactor,
+    success: (response: JSONObject) -> Unit,
+    error: (TwilioVerifyException) -> Unit
+  ) {
+    try {
+      val requestHelper = RequestHelper(context, authorization)
+      val request = Request.Builder(
+          requestHelper,
+          getChallengeURL(sid, pushFactor)
+      )
+          .httpMethod(Get)
+          .build()
+      networkProvider.execute(request, {
+        success(JSONObject(it))
+      }, { exception ->
+        error(TwilioVerifyException(exception, NetworkError))
+      })
+    } catch (e: Exception) {
+      error(TwilioVerifyException(NetworkException(e), NetworkError))
+    }
+  }
+
   private fun updateChallengeURL(challenge: FactorChallenge) =
     challenge.factor?.let { factor ->
       updateChallengeURL.replace(serviceSidPath, factor.serviceSid, true)
@@ -68,4 +97,14 @@ internal class ChallengeAPIClient(
     mapOf(
         authPayloadParam to authPayload
     )
+
+  private fun getChallengeURL(
+    challengeSid: String,
+    factor: PushFactor
+  ) = getChallengeURL.replace(serviceSidPath, factor.serviceSid, true)
+      .replace(
+          entitySidPath, factor.entitySid, true
+      )
+      .replace(challengeSidPath, challengeSid)
+
 }
