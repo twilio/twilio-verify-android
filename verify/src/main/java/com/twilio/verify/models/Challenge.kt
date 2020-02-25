4b87a6955432d0c94f2cb89e96ea057c392c77ef
Yeimi Moreno <ymoreno@twilio.com>
2020-02-25 14:30:38 -0500

    Include challenge fields. Challenge mapper

--- verify/src/main/java/com/twilio/verify/models/Challenge.kt
+++ verify/src/main/java/com/twilio/verify/models/Challenge.kt
@@ -3,12 +3,17 @@
  */
 package com.twilio.verify.models
 
+import java.util.Date
+
 interface Challenge {
   val sid: String
-  val details: LinkedHashMap<String, String>
+  val challengeDetails: ChallengeDetails
   val hiddenDetails: String
   val factorSid: String
-  var status: ChallengeStatus
+  val status: ChallengeStatus
+  val createdAt: Date
+  val updatedAt: Date
+  val expirationDate: Date
 }
 
 enum class ChallengeStatus {
@@ -17,3 +22,14 @@ enum class ChallengeStatus {
   Denied,
   Expired
 }
+
+data class ChallengeDetails(
+  val message: String,
+  val fields: List<Detail>,
+  val date: Date?
+)
+
+data class Detail(
+  val label: String,
+  val value: String
+)
