76c4eee3c7b3512a67257cef66f4c8ab1f1259be
Yeimi Fuquen <38441569+yafuquen@users.noreply.github.com>
2020-02-26 17:06:52 -0500

    ACCSEC-16829 Push challenge processor (#40)
    Add support for success without parameters in execute. Create push challenge processor

--- verify/src/main/java/com/twilio/verify/threading/Executor.kt
+++ verify/src/main/java/com/twilio/verify/threading/Executor.kt
@@ -5,43 +5,60 @@ package com.twilio.verify.threading
 
 import android.os.Handler
 import android.os.Looper
+import com.twilio.verify.TwilioVerifyException
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+typealias SafeSuccessResult<T> = (T) -> Unit
+typealias SafeError<E> = (E) -> Unit
+typealias SafeSuccess = () -> Unit
+
 private val executorService: ExecutorService = Executors.newFixedThreadPool(10)
 
-fun <T, TwilioVerifyException> execute(
+fun <T> execute(
   success: (T) -> Unit,
   error: (TwilioVerifyException) -> Unit,
-  block: (success: (T) -> Unit, error: (TwilioVerifyException) -> Unit) -> Unit
+  block: (onSuccess: SafeSuccessResult<T>, onError: SafeError<TwilioVerifyException>) -> Unit
 ) {
   executorService.execute(Task(block, success, error))
 }
 
-class Task<T, TwilioVerifyException>(
-  private val block: (success: (T) -> Unit, error: (TwilioVerifyException) -> Unit) -> Unit,
-  private val onSuccess: (T) -> Unit,
-  private val onError: (TwilioVerifyException) -> Unit,
+fun execute(
+  success: () -> Unit,
+  error: (TwilioVerifyException) -> Unit,
+  block: (onSuccess: SafeSuccess, onError: SafeError<TwilioVerifyException>) -> Unit
+) {
+  executorService.execute(
+      Task<Unit, TwilioVerifyException>({ onSuccess, onError ->
+        block(
+            { onSuccess(Unit) },
+            onError
+        )
+      }, { success() }, error)
+  )
+}
+
+class Task<T, E : Exception>(
+  private val block: (onSuccess: SafeSuccessResult<T>, onError: SafeError<E>) -> Unit,
+  private val success: (T) -> Unit,
+  private val error: (E) -> Unit,
   private val handler: Handler? = Looper.myLooper()?.takeIf { it == Looper.getMainLooper() }?.let {
     Handler(it)
   }
 ) : Runnable {
   override fun run() {
-    block(
-        { result -> success(result) },
-        { exception -> error(exception) }
-    )
+    block(::safeSuccess, ::safeError)
   }
 
-  private fun success(result: T) {
+  private fun safeSuccess(result: T) {
     handler?.let {
-      handler.post { onSuccess(result) }
-    } ?: run { onSuccess(result) }
+      handler.post { success(result) }
+    } ?: run { success(result) }
   }
 
-  private fun error(exception: TwilioVerifyException) {
+  private fun safeError(exception: E) {
     handler?.let {
-      handler.post { onError(exception) }
-    } ?: run { onError(exception) }
+      handler.post { error(exception) }
+    } ?: run { error(exception) }
   }
 }
