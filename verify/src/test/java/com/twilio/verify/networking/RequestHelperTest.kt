d12ec030b642ee25b2e50623639aac215ef3ba55
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-05-14 10:32:00 -0500

    Added header to delete http method

--- verify/src/test/java/com/twilio/verify/networking/RequestHelperTest.kt
+++ verify/src/test/java/com/twilio/verify/networking/RequestHelperTest.kt
@@ -4,6 +4,7 @@ package com.twilio.verify.networking
  * Copyright (c) 2020, Twilio Inc.
  */
 import androidx.test.core.app.ApplicationProvider
+import com.twilio.verify.networking.HttpMethod.Delete
 import com.twilio.verify.networking.HttpMethod.Get
 import com.twilio.verify.networking.HttpMethod.Post
 import org.junit.Assert.assertEquals
@@ -34,6 +35,18 @@ class RequestHelperTest {
     val commonHeaders = requestHelper.commonHeaders(Get)
     assertEquals(4, commonHeaders.size)
     assertEquals(MediaTypeValue.UrlEncoded.type, commonHeaders[MediaTypeHeader.Accept.type])
+    assertEquals(MediaTypeValue.UrlEncoded.type, commonHeaders[MediaTypeHeader.ContentType.type])
+    assertTrue(commonHeaders.containsKey(userAgent))
+    assertTrue(commonHeaders.containsKey(AuthorizationHeader))
+  }
+
+  @Test
+  fun `CommonHeaders with Delete http method should contain 4 pairs`() {
+    val authorization = BasicAuthorization("username", "password")
+    val requestHelper = RequestHelper(ApplicationProvider.getApplicationContext(), authorization)
+    val commonHeaders = requestHelper.commonHeaders(Delete)
+    assertEquals(4, commonHeaders.size)
+    assertEquals(MediaTypeValue.Json.type, commonHeaders[MediaTypeHeader.Accept.type])
     assertEquals(MediaTypeValue.UrlEncoded.type, commonHeaders[MediaTypeHeader.ContentType.type])
     assertTrue(commonHeaders.containsKey(userAgent))
     assertTrue(commonHeaders.containsKey(AuthorizationHeader))
