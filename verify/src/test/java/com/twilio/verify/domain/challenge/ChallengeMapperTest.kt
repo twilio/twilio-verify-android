4a60c4567b5789bb60a593f9b4944eabeeb7b18a
Yeimi Moreno <ymoreno@twilio.com>
2020-05-29 15:10:14 -0500

    Add tests

--- verify/src/test/java/com/twilio/verify/domain/challenge/ChallengeMapperTest.kt
+++ verify/src/test/java/com/twilio/verify/domain/challenge/ChallengeMapperTest.kt
@@ -209,6 +209,126 @@ class ChallengeMapperTest {
   }
 
   @Test
+  fun `Map a valid response from API with approved status should return a challenge`() {
+    val jsonObject = JSONObject().apply {
+      put(sidKey, "sid123")
+      put(factorSidKey, "factorSid123")
+      put(createdDateKey, "2020-02-19T16:39:57-08:00")
+      put(updatedDateKey, "2020-02-21T18:39:57-08:00")
+      put(statusKey, ChallengeStatus.Approved.value)
+      put(detailsKey, JSONObject().apply {
+        put(messageKey, "message123")
+        put(fieldsKey, JSONArray().apply {
+          put(0, JSONObject().apply {
+            put(labelKey, "label123")
+            put(valueKey, "value123")
+          })
+        })
+        put(dateKey, "2020-02-19T16:39:57-08:00")
+      }
+          .toString())
+      put(hiddenDetailsKey, JSONObject().apply {
+        put("key1", "value1")
+      }
+          .toString())
+      put(expirationDateKey, "2020-02-27T08:50:57-08:00")
+    }
+    val expectedSignatureFieldsHeader = jsonObject.keys()
+        .asSequence()
+        .toList()
+        .joinToString(
+            signatureFieldsHeaderSeparator
+        )
+    val challenge =
+      challengeMapper.fromApi(jsonObject, expectedSignatureFieldsHeader) as FactorChallenge
+    assertEquals(jsonObject.getString(sidKey), challenge.sid)
+    assertEquals(jsonObject.getString(factorSidKey), challenge.factorSid)
+    assertNull(challenge.signatureFields)
+    assertEquals(fromRFC3339Date(jsonObject.getString(createdDateKey)), challenge.createdAt)
+    assertEquals(fromRFC3339Date(jsonObject.getString(updatedDateKey)), challenge.updatedAt)
+    assertEquals(jsonObject.getString(statusKey), challenge.status.value)
+    assertNull(challenge.response)
+    val details = JSONObject(jsonObject.getString(detailsKey))
+    assertEquals(details.getString(messageKey), challenge.challengeDetails.message)
+    assertEquals(
+        details.getJSONArray(fieldsKey)
+            .length(), challenge.challengeDetails.fields.size
+    )
+    assertEquals(
+        details.getJSONArray(fieldsKey)
+            .getJSONObject(0)
+            .getString(labelKey),
+        challenge.challengeDetails.fields[0].label
+    )
+    assertEquals(
+        details.getJSONArray(fieldsKey)
+            .getJSONObject(0)
+            .getString(valueKey),
+        challenge.challengeDetails.fields[0].value
+    )
+    assertEquals(fromRFC3339Date(details.getString(dateKey)), challenge.challengeDetails.date)
+    assertEquals(jsonObject.getString(hiddenDetailsKey), challenge.hiddenDetails)
+    assertEquals(fromRFC3339Date(jsonObject.getString(expirationDateKey)), challenge.expirationDate)
+  }
+
+  @Test
+  fun `Map a valid response from API with pending status and no signature fields should return a challenge`() {
+    val jsonObject = JSONObject().apply {
+      put(sidKey, "sid123")
+      put(factorSidKey, "factorSid123")
+      put(createdDateKey, "2020-02-19T16:39:57-08:00")
+      put(updatedDateKey, "2020-02-21T18:39:57-08:00")
+      put(statusKey, ChallengeStatus.Pending.value)
+      put(detailsKey, JSONObject().apply {
+        put(messageKey, "message123")
+        put(fieldsKey, JSONArray().apply {
+          put(0, JSONObject().apply {
+            put(labelKey, "label123")
+            put(valueKey, "value123")
+          })
+        })
+        put(dateKey, "2020-02-19T16:39:57-08:00")
+      }
+          .toString())
+      put(hiddenDetailsKey, JSONObject().apply {
+        put("key1", "value1")
+      }
+          .toString())
+      put(expirationDateKey, "2020-02-27T08:50:57-08:00")
+    }
+    val challenge =
+      challengeMapper.fromApi(jsonObject) as FactorChallenge
+    assertEquals(jsonObject.getString(sidKey), challenge.sid)
+    assertEquals(jsonObject.getString(factorSidKey), challenge.factorSid)
+    assertNull(challenge.signatureFields?.joinToString(signatureFieldsHeaderSeparator))
+    assertEquals(fromRFC3339Date(jsonObject.getString(createdDateKey)), challenge.createdAt)
+    assertEquals(fromRFC3339Date(jsonObject.getString(updatedDateKey)), challenge.updatedAt)
+    assertEquals(jsonObject.getString(statusKey), challenge.status.value)
+    assertNull(challenge.response)
+    val details = JSONObject(jsonObject.getString(detailsKey))
+    assertEquals(details.getString(messageKey), challenge.challengeDetails.message)
+    assertEquals(
+        details.getJSONArray(fieldsKey)
+            .length(), challenge.challengeDetails.fields.size
+    )
+    assertEquals(
+        details.getJSONArray(fieldsKey)
+            .getJSONObject(0)
+            .getString(labelKey),
+        challenge.challengeDetails.fields[0].label
+    )
+    assertEquals(
+        details.getJSONArray(fieldsKey)
+            .getJSONObject(0)
+            .getString(valueKey),
+        challenge.challengeDetails.fields[0].value
+    )
+    assertEquals(fromRFC3339Date(details.getString(dateKey)), challenge.challengeDetails.date)
+    assertEquals(jsonObject.getString(hiddenDetailsKey), challenge.hiddenDetails)
+    assertEquals(fromRFC3339Date(jsonObject.getString(expirationDateKey)), challenge.expirationDate)
+  }
+
+  @Test
   fun `Map a response from API without sid should throw an error`() {
     val jsonObject = JSONObject().apply {
       put(factorSidKey, "factorSid123")
