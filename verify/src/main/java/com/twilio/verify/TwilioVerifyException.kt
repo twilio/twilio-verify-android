d4b45c77a0ec10ffaaa6d442e67441cd1fd38761
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-06-16 18:59:11 -0500

    Using conts

--- verify/src/main/java/com/twilio/verify/TwilioVerifyException.kt
+++ verify/src/main/java/com/twilio/verify/TwilioVerifyException.kt
@@ -3,18 +3,25 @@
  */
 package com.twilio.verify
 
+private const val NETWORK_CODE = 68001
+private const val MAPPER_CODE = 68002
+private const val STORAGE_CODE = 68003
+private const val INPUT_CODE = 68004
+private const val KEY_STORAGE_CODE = 68005
+private const val INITIALIZATION_CODE = 68006
+private const val AUTHENTICATION_TOKEN_CODE = 68007
+
 class TwilioVerifyException(
   cause: Throwable,
   errorCode: ErrorCode
 ) : Exception(errorCode.message, cause) {
-
-  enum class ErrorCode(val message: String, val code: Int) {
-    NetworkError("(${NetworkError.code}) Exception while calling API", 68001),
-    MapperError("(${MapperError.code}) Exception while mapping an entity", 68002),
-    StorageError("(${StorageError.code}) Exception while storing/loading an entity", 68003),
-    InputError("(${InputError.code}) Exception while loading input", 68004),
-    KeyStorageError("(${KeyStorageError.code}) Exception while storing/loading key pairs", 68005),
-    InitializationError("(${InitializationError.code}) Exception while initializing", 68006),
-    AuthenticationTokenError("(${AuthenticationTokenError.code}) Exception while generating token", 68007)
+  enum class ErrorCode(val message: String) {
+    NetworkError("{$NETWORK_CODE} Exception while calling API"),
+    MapperError("{$MAPPER_CODE} Exception while mapping an entity"),
+    StorageError("{$STORAGE_CODE} Exception while storing/loading an entity"),
+    InputError("{$INPUT_CODE} Exception while loading input"),
+    KeyStorageError("{$KEY_STORAGE_CODE} Exception while storing/loading key pairs"),
+    InitializationError("{$INITIALIZATION_CODE} Exception while initializing"),
+    AuthenticationTokenError("{$AUTHENTICATION_TOKEN_CODE} Exception while generating token")
   }
 }
