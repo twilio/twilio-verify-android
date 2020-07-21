8feff66901a3864318350ba2546695db3610f7f0
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-07-21 15:28:48 -0500

    internal class BaseAPIClient

--- verify/src/main/java/com/twilio/verify/api/BaseAPIClient.kt
+++ verify/src/main/java/com/twilio/verify/api/BaseAPIClient.kt
@@ -6,7 +6,7 @@ import com.twilio.verify.data.DateProvider
  * Copyright (c) 2020, Twilio Inc.
  */
 
-open class BaseAPIClient(private val dateProvider: DateProvider) {
+internal open class BaseAPIClient(private val dateProvider: DateProvider) {
   fun syncTime(
     date: String
   ) {
