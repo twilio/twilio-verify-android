8b71268c62ce955099c00e9144eeb4dca2919ec2
Yeimi Moreno <ymoreno@twilio.com>
2020-08-18 19:31:37 -0500

    Remove dependency. Organize readme links

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -71,7 +71,6 @@ dependencies {
   testImplementation("org.mockito:mockito-inline:2.28.2")
   testImplementation("androidx.test:core:1.2.0")
   testImplementation("com.squareup.okhttp3:mockwebserver:4.3.1")
-  testImplementation("org.mockito:mockito-core:3.1.0")
   androidTestImplementation("androidx.test.ext:junit:1.1.1")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
