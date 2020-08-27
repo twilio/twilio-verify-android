c50515890a196eac421d766bb74675d53e344086
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-08-27 18:06:00 -0500

    Test

--- scripts/commit-msg.sh
+++ scripts/commit-msg.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 echo "Validating commit message convention"
 
-commit_regex='^(docs|fix|feat|chore|style|refactor|perf|test)(?:\((.*)\))?(!?)\: (.*)'
+commit_regex='^(docs|fix|feat|chore|refactor|perf|test)(?:\((.*)\))?(!?)\: (.*)'
 if ! grep -iqE "$commit_regex" "$1"; then
     echo "*********************************************"
     echo "      Please use conventional commits        "
-    echo "          Use structural elements:           "
-    echo "     docs|fix|feat|chore|style|refactor      "
-    echo "          perf|BREAKING CHANGE               "
+    echo "           Structural elements:              "
+    echo "     docs|fix|feat|chore|refactor|perf       "
     echo "*********************************************"
     exit 1
 fi
