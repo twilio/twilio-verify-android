9a90c435841408c4b0abd02d22ef3d4e491f1977
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-08-27 18:11:04 -0500

    test [skip ci]

--- scripts/commit-msg.sh
+++ scripts/commit-msg.sh
@@ -6,7 +6,7 @@ if ! grep -iqE "$commit_regex" "$1"; then
     echo "*********************************************"
     echo "      Please use conventional commits        "
     echo "           Structural elements:              "
-    echo "     docs|fix|feat|chore|refactor|perf       "
+    echo "   docs|fix|feat|chore|refactor|perf|test    "
     echo "*********************************************"
     exit 1
 fi
