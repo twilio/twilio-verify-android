0949945ac86fbeaa5d48c84deff882b5b0ed8052
Sergio Fierro Perdomo <sergiofierro.perdomo@gmail.com>
2020-09-28 09:43:34 -0500

    [ACCSEC-19012] Add copyright header to files

--- sample/src/main/java/com/twilio/verify/sample/push/FirebasePushService.kt
+++ sample/src/main/java/com/twilio/verify/sample/push/FirebasePushService.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Twilio Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.twilio.verify.sample.push
 
 import android.app.NotificationChannel
@@ -20,10 +36,6 @@ import com.twilio.verify.sample.view.MainActivity
 import com.twilio.verify.sample.view.challenges.update.ARG_CHALLENGE_SID
 import com.twilio.verify.sample.view.challenges.update.ARG_FACTOR_SID
 import org.koin.android.ext.android.inject
-
-/*
- * Copyright (c) 2020, Twilio Inc.
- */
 
 internal const val typeKey = "type"
 internal const val factorSidKey = "factor_sid"
--- verify/src/main/java/com/twilio/verify/domain/service/ServiceFacade.kt
+++ verify/src/main/java/com/twilio/verify/domain/service/ServiceFacade.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Twilio Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.twilio.verify.domain.service
 
 import android.content.Context
@@ -9,10 +25,6 @@ import com.twilio.verify.models.Service
 import com.twilio.verify.networking.Authentication
 import com.twilio.verify.networking.NetworkProvider
 import com.twilio.verify.threading.execute
-
-/*
- * Copyright (c) 2020, Twilio Inc.
- */
 
 internal class ServiceFacade(
   private val serviceProvider: ServiceProvider,
