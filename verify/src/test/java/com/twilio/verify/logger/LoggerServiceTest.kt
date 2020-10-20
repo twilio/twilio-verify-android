57379e03a9499f723d8175d9ef0392f3c5b5415f
Yeimi Moreno <ymoreno@twilio.com>
2020-10-20 15:37:56 -0500

    Improve coverage

---
+++ verify/src/test/java/com/twilio/verify/logger/LoggerServiceTest.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020, Twilio Inc.
+ */
+package com.twilio.verify.logger
+
+import com.nhaarman.mockitokotlin2.spy
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class LoggerServiceTest {
+
+  @Test
+  fun `Setting log level returns correct level`() {
+    val logLevel = LogLevel.Info
+    val loggerService: LoggerService = TestLoggerService(logLevel)
+    assertEquals(logLevel.level, loggerService.level)
+  }
+
+  @Test
+  fun `Call log service method calls logger service method`() {
+    val logLevel = LogLevel.Info
+    val message = "Message"
+    val loggerService = spy(TestLoggerService(logLevel))
+    loggerService.log(logLevel.level, message)
+    verify(loggerService).log(logLevel, message)
+  }
+}
+
+class TestLoggerService(override val logLevel: LogLevel) : LoggerService {
+
+  override fun log(
+    logLevel: LogLevel,
+    message: String,
+    throwable: Throwable?
+  ) {
+  }
+}
