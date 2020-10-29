d12d1c84875ef9e83a96e1906910f9103692bb1c
Yeimi Moreno <ymoreno@twilio.com>
2020-10-28 19:04:17 -0500

    Fix test

--- verify/src/androidTest/java/com/twilio/verify/ClearLocalDataTests.kt
+++ verify/src/androidTest/java/com/twilio/verify/ClearLocalDataTests.kt
@@ -3,6 +3,7 @@
  */
 package com.twilio.verify
 
+import com.twilio.verify.domain.factor.models.PushFactor
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -10,11 +11,26 @@ import org.junit.Test
 class ClearLocalDataTests : BaseFactorTest() {
 
   @Test
+  fun testClearLocalDataWithFactorShouldDeleteAndCallThen() {
+    assertTrue(keyStore.containsAlias(factor!!.keyPairAlias))
+    assertTrue(encryptedSharedPreferences.contains(getFactorKey(factor!!)))
+    idlingResource.increment()
+    twilioVerify.clearLocalData {
+      assertTrue(encryptedSharedPreferences.all.isEmpty())
+      assertFalse(keyStore.containsAlias(factor!!.keyPairAlias))
+      assertFalse(encryptedSharedPreferences.contains(getFactorKey(factor!!)))
+      idlingResource.decrement()
+    }
+    idlingResource.waitForResource()
+  }
+
+  @Test
   fun testClearLocalDataWithFactorsShouldDeleteAndCallThen() {
-    val factors = mutableListOf(factor!!)
-    repeat(2) {
-      createFactor().run {
-        factors.add(factor!!)
+    val factors = mutableListOf<PushFactor>().apply {
+      repeat(2) {
+        createFactor(sid = "factorSid$it") { factor ->
+          add(factor as PushFactor)
+        }
       }
     }
     factors.forEach { factor ->
@@ -23,12 +39,12 @@ class ClearLocalDataTests : BaseFactorTest() {
     }
     idlingResource.increment()
     twilioVerify.clearLocalData {
+      assertTrue(encryptedSharedPreferences.all.isEmpty())
       factors.forEach { factor ->
         assertFalse(keyStore.containsAlias(factor.keyPairAlias))
         assertFalse(encryptedSharedPreferences.contains(getFactorKey(factor)))
-        assertTrue(encryptedSharedPreferences.all.isEmpty())
-        idlingResource.decrement()
       }
+      idlingResource.decrement()
     }
     idlingResource.waitForResource()
   }
