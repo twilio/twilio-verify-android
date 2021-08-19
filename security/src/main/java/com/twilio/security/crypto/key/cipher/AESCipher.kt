a81ca533027c889e1fd0f925c9321fd74bf6e9bc
Yeimi Moreno <ymoreno@twilio.com>
2021-08-19 18:15:19 -0500

    Handle error in cipher when encrypting/decrypting with authentication

--- security/src/main/java/com/twilio/security/crypto/key/cipher/AESCipher.kt
+++ security/src/main/java/com/twilio/security/crypto/key/cipher/AESCipher.kt
@@ -21,6 +21,7 @@ import com.twilio.security.crypto.KeyException
 import com.twilio.security.crypto.key.authentication.Authenticator
 import com.twilio.security.logger.Level
 import com.twilio.security.logger.Logger
+import javax.crypto.Cipher
 import javax.crypto.SecretKey
 
 class AESCipher(
@@ -29,7 +30,7 @@ class AESCipher(
   private val androidKeyStoreOperations: AndroidKeyStoreOperations
 ) : com.twilio.security.crypto.key.cipher.Cipher {
   override fun encrypt(data: ByteArray): EncryptedData {
-    return try {
+    try {
       return androidKeyStoreOperations.encrypt(data, cipherAlgorithm, key)
     } catch (e: Exception) {
       Logger.log(Level.Error, e.toString(), e)
@@ -37,12 +38,17 @@ class AESCipher(
     }
   }
 
-  override fun encrypt(data: ByteArray, authenticator: Authenticator, success: (EncryptedData) -> Unit, error: (Exception) -> Unit) {
+  override fun encrypt(
+    data: ByteArray,
+    authenticator: Authenticator,
+    success: (EncryptedData) -> Unit,
+    error: (Exception) -> Unit
+  ) {
     try {
       authenticator.startAuthentication(
         androidKeyStoreOperations.getCipherForEncryption(cipherAlgorithm, key),
         {
-          success(androidKeyStoreOperations.encrypt(data, it))
+          encrypt(data, it, success, error)
         },
         error
       )
@@ -52,7 +58,7 @@ class AESCipher(
   }
 
   override fun decrypt(data: EncryptedData): ByteArray {
-    return try {
+    try {
       return androidKeyStoreOperations.decrypt(data, cipherAlgorithm, key)
     } catch (e: Exception) {
       Logger.log(Level.Error, e.toString(), e)
@@ -60,15 +66,46 @@ class AESCipher(
     }
   }
 
-  override fun decrypt(data: EncryptedData, authenticator: Authenticator, success: (ByteArray) -> Unit, error: (Exception) -> Unit) {
+  override fun decrypt(
+    data: EncryptedData,
+    authenticator: Authenticator,
+    success: (ByteArray) -> Unit,
+    error: (Exception) -> Unit
+  ) {
     try {
       authenticator.startAuthentication(
         androidKeyStoreOperations.getCipherForDecryption(cipherAlgorithm, key, data),
         {
-          success(androidKeyStoreOperations.decrypt(data, it))
+          decrypt(data, it, success, error)
         },
         error
       )
+    } catch (e: Exception) {
+      error(e)
+    }
+  }
+
+  private fun encrypt(
+    data: ByteArray,
+    cipherObject: Cipher,
+    success: (EncryptedData) -> Unit,
+    error: (Exception) -> Unit
+  ) {
+    try {
+      success(androidKeyStoreOperations.encrypt(data, cipherObject))
+    } catch (e: Exception) {
+      error(e)
+    }
+  }
+
+  private fun decrypt(
+    data: EncryptedData,
+    cipherObject: Cipher,
+    success: (ByteArray) -> Unit,
+    error: (Exception) -> Unit
+  ) {
+    try {
+      success(androidKeyStoreOperations.decrypt(data, cipherObject))
     } catch (e: Exception) {
       error(e)
     }
